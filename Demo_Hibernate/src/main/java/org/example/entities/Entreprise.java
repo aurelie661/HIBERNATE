b3457020e130a45c15705b19ae970bd20ef8ae03
aurelie661 <boutaurel59@gmail.com>
2023-08-28 15:19:11 +0200

    Demo Hibernate + exo 1

--- Demo_Hibernate/src/main/java/org/example/entities/Entreprise.java
+++ Demo_Hibernate/src/main/java/org/example/entities/Entreprise.java
@@ -11,18 +11,18 @@ public class Entreprise {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     private String nom;
 
     @OneToMany(mappedBy = "entreprise")
     private List<Personne> personnes = new ArrayList<>();
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
